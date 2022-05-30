bb5a2fcc9ba4f5b43d800af331cd941df62fecb6
CyberHard <97868448+CyberHard@users.noreply.github.com>
2022-05-30 19:04:27 +0500

    Update bot_3.js

---
+++ data/script/group_1/bot_3.js
@@ -0,0 +1,8 @@
+const mineflayer = require('mineflayer')
+
+const bot = mineflayer.createBot({
+  host: 'Tigir313.aternos.me', // minecraft server ip
+  username: '_Calamity', // minecraft username
+  port: 25565,                // only set if you need a port that isn't 25565
+  
+})
